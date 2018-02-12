5ff975ec11cc6bc0c4d7cc582d54d3f3a10f5bc8
Max Milton <max@wearegenki.com>
2018-02-12 21:52:40 +1100

    Initial support for all input types + other improvements

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
-'use strict'; // eslint-disable-line
+/**
+ * marko-bind
+ * @overview An easy way to do reactive data binding in MarkoJS.
+ * @author Max Milton <max@wearegenki.com>
+ *
+ * Copyright 2018 We Are Genki
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
 
 const runtimeModulePath = require.resolve('./runtime');
 const runtimeDynamicModulePath = require.resolve('./runtime-dynamic');
 
-const dynamicEls = [];
+const dynamicallyBound = [];
 let dynamicRuntimeLoaded = false;
 
 module.exports = function transform(el, context) {
@@ -18,6 +38,12 @@ module.exports = function transform(el, context) {
   // figure out what the attribute refers to
   let toExpression = builder.parseExpression(attribute.argument);
 
+  // console.log('\n@@--------------------------------------------------------@@');
+  // console.log('@@ ATTR', attribute);
+  // console.log('@@ EXPRESSION', toExpression);
+  // console.log('@@ EL', el);
+  // console.log('@@--------------------------------------------------------@@\n');
+
   if (toExpression.type === 'Literal') {
     // TODO: Resolve nested state
     //  ↳ Check if marko already has something for this
@@ -28,8 +54,7 @@ module.exports = function transform(el, context) {
   } else if (toExpression.type !== 'MemberExpression') {
     // dynamic attribute expression which must be handled at runtime
 
-    // console.log('@@ EL', el);
-    dynamicEls.push(el);
+    dynamicallyBound.push(el);
 
     // create an attribute we can parse at runtime
     el.setAttributeValue('__bind', builder.literal(toExpression.name));
@@ -41,38 +66,90 @@ module.exports = function transform(el, context) {
       // call the runtime setup module after all the nodes are in place
       const astNodes = context.root.body.array;
       const lastNode = astNodes[astNodes.length - 1];
-      console.log('dynamicEls', dynamicEls);
+      // console.log('dynamicallyBound', dynamicallyBound);
       lastNode.appendChild(builder.node(() =>
         builder.functionCall(builder.identifier('__bindDynamic'), [
           builder.identifier('component'),
-          builder.arrayExpression(dynamicEls),
-        ]),
+          builder.arrayExpression(dynamicallyBound),
+        ])
       )); // eslint-disable-line function-paren-newline
+
+      // call the runtime setup module before any dynamic bind() inputs
+      // console.log('dynamicallyBound', dynamicallyBound);
+      // el.prependChild(builder.node(() =>
+      // // el.appendChild(builder.node(() =>
+      //   builder.functionCall(builder.identifier('__bindDynamic'), [
+      //     builder.identifier('component'),
+      //     builder.arrayExpression(dynamicallyBound),
+      //   ])
+      // ));
 
       dynamicRuntimeLoaded = true;
     }
 
-    return; // exit early because the rest is for compile-time setup
+    return; // exit early because the rest is for non-dynamic element setup
   }
-
-  // FIXME: Handle the various input types; input, select, textarea, etc.
-
-  el.setAttributeValue('value', toExpression);
 
   context.importModule('__bind', runtimeModulePath);
 
   // set up event binding
   const eventMethod = builder.memberExpression(
     builder.identifier('__component'),
-    builder.identifier('d'),
+    builder.identifier('d')
   );
   const eventArgs = [
-    builder.identifier('__bind'), // this is run when events emit
+    builder.identifier('__bind'), // this is run when the event emits
     builder.arrayExpression([
-      builder.literal(toExpression.property.name),
+      builder.identifier('component'),
+      builder.literal(toExpression.property.name), // state object key
+      attribute.value || 'null', // optional callback
     ]),
   ];
   const propValue = builder.functionCall(eventMethod, eventArgs);
 
-  el.setPropertyValue('oninput', propValue, false);
+  function checkBindConflicts(attribute, event) {
+    // TODO
+    const conflict = false;
+    if (conflict) console.log('x');
+  }
+
+  /** Handle cases for the various input types */
+
+  const tag = el.tagName;
+  const type = el.getAttribute('type');
+
+  // console.log('####  TAG', tag);
+  // console.log('#### TYPE', type);
+
+  if (tag === 'select') {
+    // select
+    checkBindConflicts('value', 'onchange');
+    /**
+     * NOTE: The "value" attribute doesn't actually do anything, it's necessary to
+     * set el.value at runtime to correctly update the input.
+     */
+    // const attrValue = el.getAttribute('multiple')
+    //   ? `${toExpression}[0]`
+    //   : toExpression;
+    // el.setAttributeValue('value', attrValue);
+    el.setPropertyValue('onchange', propValue, false);
+  } else if (tag === 'input' && type === 'checkbox') {
+    // checkbox
+    checkBindConflicts('checked', 'onchange');
+    el.setAttributeValue('checked', toExpression);
+    el.setPropertyValue('onchange', propValue, false);
+  } else if (tag === 'input' && type === 'radio') {
+    // radio
+    // XXX: Currently the same as checkbox.
+    checkBindConflicts('checked', 'onchange');
+    el.setAttributeValue('checked', toExpression);
+    el.setPropertyValue('onchange', propValue, false);
+  } else if (tag === 'input' || tag === 'textarea') {
+    // generic input + textarea
+    checkBindConflicts('value', 'oninput');
+    el.setAttributeValue('value', toExpression);
+    el.setPropertyValue('oninput', propValue, false);
+  } else {
+    throw new Error(`${el.tagString}\n ↳ "bind()" is not supported on this element type.`);
+  }
 };
--- src/runtime.js
+++ src/runtime.js
@@ -1,8 +1,58 @@
-// TODO: Handle the various inputs and their edge cases
-//  ↳ REF: https://github.com/vuejs/vue/blob/master/src/platforms/web/compiler/directives/model.js
+'use strict';
 
-'use strict'; // eslint-disable-line
+/**
+ * Bind the value from an input event to a component state.
+ * @param {MarkoComponent} component The parent Marko component class.
+ * @param {string} key Key of the state data object to update.
+ * @param {Function} [cb] Optional function to run on input events.
+ * @param {Event} event An event emit by the bound input element.
+ * @param {(HTMLInputElement|HTMLSelectElement|HTMLTextAreaElement)} el The input
+ * element which emit the event.
+ */
+module.exports = function bind(component, key, cb, event, el) {
+  // console.log('\n//------------------------------------------//');
+  // console.log('RUNTIME COMP', component);
+  // console.log('RUNTIME  KEY', key);
+  // console.log('RUNTIME   CB', cb);
+  // console.log('RUNTIME EVNT', event);
+  // console.log('RUNTIME   EL', el);
+  // console.log('RUNTIME  TAG', el.tagName);
+  // console.log('RUNTIME TYPE', el.type);
+  // console.log('//------------------------------------------//\n');
 
-module.exports = function bind(key, event) {
-  this.setState(key, event.target.value);
+  var value;
+  var selected;
+
+  if (el.tagName === 'SELECT') {
+    // select
+    // XXX: el.selectedOptions would be better but isn't supported in IE
+    value = []
+      .filter.call(el.options, function (opt) { return opt.selected; })
+      .map(function (opt) { return opt.value; });
+
+    selected = value[0];
+
+    if (!el.multiple) value = value[0];
+  } else if (el.type === 'checkbox') {
+    // checkbox
+    value = el.checked;
+  } else if (el.type === 'radio') {
+    // radio
+    // FIXME: Merge with checkbox if they're the same
+    value = el.checked;
+  } else {
+    // generic input and textarea
+    value = el.value;
+  }
+
+  component.setState(key, value);
+
+  if (selected) {
+    // without update() the value assignment doesn't work
+    component.update();
+    el.value = selected; // eslint-disable-line no-param-reassign
+  }
+
+  // run callback function (with context this = component)
+  if (typeof cb === 'function') cb.call(component);
 };
