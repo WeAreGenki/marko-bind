4ab9d5912d07dabab0a86328ffac4a3d449a132b
Max Milton <max@wearegenki.com>
2018-03-04 13:08:03 +1100

    Add initial travis setup + working on tests

--- test/__tests__/tests.js
+++ test/__tests__/tests.js
@@ -1,9 +1,12 @@
+require('marko/node-require');
 const { renderMarkoComponent } = require('@wearegenki/test/lib/marko');
 const inputText = require('./components/input-text.marko');
-// import inputSelect = require('./components/input-select.marko');
+// const inputSelect = require('./components/input-select.marko');
 
 test('marko component rendered correctly', () => {
+  console.log('### 00', renderMarkoComponent);
+  console.log('### 11', inputText);
   const component = renderMarkoComponent(inputText);
-  console.log('###', component);
+  console.log('### 22', component);
   expect(component.state.text).toBeDefined();
 });
--- test/jest.config.js
+++ test/jest.config.js
@@ -5,7 +5,6 @@ const path = require('path');
 module.exports = {
   preset: '@wearegenki/test',
   rootDir: path.resolve(__dirname, '..'),
-  mapCoverage: true,
   coverageDirectory: '<rootDir>/test/coverage',
   transform: {}, // don't use babel to transpile
 };
