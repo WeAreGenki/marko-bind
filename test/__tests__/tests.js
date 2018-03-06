3813d82b15a141979a0a65e4f1a959de8a754576
Max Milton <max@wearegenki.com>
2018-03-07 09:29:04 +1100

    More testing setup

--- test/__tests__/tests.js
+++ test/__tests__/tests.js
@@ -1,12 +1,31 @@
-require('marko/node-require');
+// REF: https://gist.github.com/jasonmacdonald/b417d2e9dce32843cbde08fba5320cfe
+
 const { renderMarkoComponent } = require('@wearegenki/test/lib/marko');
 const inputText = require('./components/input-text.marko');
-// const inputSelect = require('./components/input-select.marko');
+const inputSelect = require('./components/input-select.marko');
 
-test('marko component rendered correctly', () => {
-  console.log('### 00', renderMarkoComponent);
-  console.log('### 11', inputText);
-  const component = renderMarkoComponent(inputText);
-  console.log('### 22', component);
+test('marko components render correctly', () => {
+  let component = renderMarkoComponent(inputText);
   expect(component.state.text).toBeDefined();
+
+  component = renderMarkoComponent(inputSelect);
+  expect(component.state.selected).toBeDefined();
+});
+
+describe('basic text input', () => {
+  let component;
+
+  beforeEach(() => {
+    // component = renderMarkoComponent(inputText);
+  });
+
+  test('event sets component state', () => {
+    component = renderMarkoComponent(inputText);
+
+    // wrapper
+    //   .find('input')
+    //   .simulate('change', { target: { value: 'My new value' }});
+
+    expect(component.state.text).toBeDefined();
+  });
 });
--- test/jest.config.js
+++ test/jest.config.js
@@ -2,9 +2,18 @@
 
 const path = require('path');
 
+const { libDir } = require('@wearegenki/test');
+
 module.exports = {
   preset: '@wearegenki/test',
   rootDir: path.resolve(__dirname, '..'),
   coverageDirectory: '<rootDir>/test/coverage',
-  transform: {}, // don't use babel to transpile
+
+  // marko specific setup
+  browser: true, // use browser field in package.json
+  resolver: `${libDir}/marko/resolver.js`,
+  transform: {
+    // '^.+\\.jsx?$': 'babel-jest', // don't use babel to transpile
+    '^.+\\.(html|marko)$': `${libDir}/marko/transform.js`,
+  },
 };
