db1298aaed3040aeffc788cbb30a8e35698ea120
Max Milton <max@wearegenki.com>
2018-03-13 07:39:15 +1100

    Work to date

--- test/BasicInputs.test.js
+++ test/BasicInputs.test.js
@@ -1,5 +1,7 @@
 'use strict'; // eslint-disable-line
 
+const { mount } = require('@wearegenki/test/lib/marko/helpers');
+
 const InputText = require('./fixtures/InputText');
 // const InputSelect = require('./fixtures/InputSelect');
 
@@ -19,23 +21,23 @@ describe('Basic text input component', () => {
     });
   });
 
-  it.skip('should have an input', async () => {
+  it('should have an input', async () => {
     const renderResult = await InputText.render({
       id: 'oom',
       label: 'aaa',
       placeholder: 'aaa',
     });
-    const browserContext = renderResult.appendTo(document.body).getComponent();
-    expect(browserContext.input).toBeDefined();
+    const context = await mount(renderResult);
+    expect(context.input).toBeDefined();
   });
 
-  it.skip('should getValue() correctly', async () => {
-    const renderResult = await InputText.render({
-      id: 'woo',
-      label: 'bbb',
-      placeholder: 'bbb',
-    });
-    const browserContext = renderResult.appendTo(document.body).getComponent();
-    expect(browserContext.getValue()).toBe('woo');
-  });
+  // it.skip('should getValue() correctly', async () => {
+  //   const renderResult = await InputText.render({
+  //     id: 'woo',
+  //     label: 'bbb',
+  //     placeholder: 'bbb',
+  //   });
+  //   const context = await mount(renderResult);
+  //   expect(context.getValue()).toBe('woo');
+  // });
 });
